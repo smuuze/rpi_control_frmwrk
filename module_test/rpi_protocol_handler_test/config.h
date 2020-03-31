634acd393d6c2209ce432dc7f0fe57269d9bb7f6
smuuze <sebastia_n@web.de>
2020-03-31 08:42:01 +0000

    CHANGE: config.h - removed unused stuff

--- module_test/rpi_protocol_handler_test/config.h
+++ module_test/rpi_protocol_handler_test/config.h
@@ -1,35 +1,6 @@
 #ifndef   _config_H_ /* parse include file only once */
 #define   _config_H_
 
-#define noTRACEAKTIV         // Schaltet generell meine TRACES EIN/AUS
-
-#define noTRACES_ENABLED
-#define noTRACE_FAKE_RTOS_TIME_STAMP 2	// Trace-Ausgaben mit Timestamp (Anzahl-Bytes 1..4)
-
-///-----------------------------------------------------------------------------
-#define EMBEDDED
-
-///-----------------------------------------------------------------------------
-#define NO_CARD_READ_DEVICE
-
-#define DEBUS_F1_HANDLER      // Testfunktionen fuer delpro
-///----------------------------------------------------------------------------
-
-///-----------------------------------------------------------------------------
-///  VERSION
-///-----------------------------------------------------------------------------
-#define MIN_BIOS_VERSION 0x163
-
-///-----------------------------------------------------------------------------
-/// Konfigurationsspeicher
-// Konfigurationsspeicher ist groesser als 110 Byte und passt nicht in den deBus
-// Buffer deswegen CFG_ALL_SUPPORT = 0
-#define CFG_ALL_SUPPORT   0 // CFG-Register 02 wg. Laengenbeschraenkung auf 110 nicht unterstuetzt
-
-// Wenn die Konfiguration groesser als 255 Byte wird, soll dieser Schalter aktiviert werden.
-#define  BIG_CFG_MEM 1
-
-#define CFG_DEFAULT_VALUE_SELECTOR 2
 ///-----------------------------------------------------------------------------
 //#ifndef __ASSEMBLER__
 //#include "platine/22B2.h"  // port definition + crystal frequency
@@ -54,24 +25,6 @@
 
 #define I2C_CLK_LIMIT 225000
 
-
-/// z.Zt nur TRUE/FALSE koennte aber mal 32 BIT CCM-MAC bedeuten:  //32
-#define noCOPRO1_ENCRYPTION           32
-
-#define ASSERT_LOOP_INDICATION_ON() \
-        BEEPER_ON();                \
-        LED_RED_ON();               \
-        LED_GRN_ON();               \
-        LED_YELLOW_OFF()
-
-#define ASSERT_LOOP_INDICATION_AFTER_3_SECONDS() \
-        BEEPER_OFF()
-
-// ================================================================================
-#define HOSTFIFO_BUFFER_SIZE 	512	//Eventuell auch kleiner m�glich
-#define AIRLINK_SERVICE_TYPE 	2	//Wenn 0: (AIRLINK_NOT_USED)  kein Airlink
-					//Wenn 1: (AIRLINK_IN_USE)    Airlink Service in diesem CPU
-					//Wenn 2: (AIRLINK_IN_COPRO1) Airlink Service im Copro
 
 //-------------------------------------------------------------------------
 #define config_LOCAL_COMMAND_HANDLER_TABLE_FUNC_PROTO		\
