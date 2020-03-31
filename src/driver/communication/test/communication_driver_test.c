6f7ebef339d046fa31d17a2738ce5b43488b51a2
smuuze <sebastia_n@web.de>
2020-03-31 15:38:56 +0100

    FIX / CHANGE: communication_test_driver - removed return statement from wait_for_tx that prevents function from normal operation, merged rx-timer tx-timer to TRX_TIMER,

--- src/driver/communication/test/communication_driver_test.c
+++ src/driver/communication/test/communication_driver_test.c
@@ -1,22 +1,27 @@
-/*! \file *********************************************************************
+ /*
+  * \@file	driver/communication/test/communication_driver_test.c
+  * \author	sebastian lesse
+  */
 
- *****************************************************************************/
+#define TRACER_OFF
+
+//-----------------------------------------------------------------------------
 
 #include "config.h"  // immer als erstes einbinden!
-#include "specific.h"
 
-#include "driver/communication/test/communication_driver_test.h"
+//-----------------------------------------------------------------------------
+
+#include "tracer.h"
+
+//-----------------------------------------------------------------------------
 
 #include "common/local_module_status.h"
 #include "common/local_mutex.h"
 #include "common/local_msg_buffer.h"
-
+#include "common/signal_slot_interface.h"
 #include "time_management/time_management.h"
 
-//---------- Implementation of Traces -----------------------------------------
-
-#define TRACER_ON
-#include "tracer.h"
+#include "driver/communication/test/communication_driver_test.h"
 
 //-----------------------------------------------------------------------------
 
@@ -32,16 +37,23 @@ BUILD_LOCAL_MSG_BUFFER( , TEST_RX_BUFFER, 128)
 
 BUILD_MODULE_STATUS_FAST(test_driver_status, 2)
 
-TIME_MGMN_BUILD_STATIC_TIMER_U16(rx_timer)
-TIME_MGMN_BUILD_STATIC_TIMER_U16(tx_timer)
+TIME_MGMN_BUILD_STATIC_TIMER_U16(TRX_TIMER)
+
+SIGNAL_SLOT_INTERFACE_CREATE_SIGNAL(SIGNAL_TEST_DRIVER_START_RX)
+SIGNAL_SLOT_INTERFACE_CREATE_SIGNAL(SIGNAL_TEST_DRIVER_START_TX)
 
 //-----------------------------------------------------------------------------
 
 
 void test_driver_initialize(void) {
 
+	DEBUG_PASS("test_driver_initialize()");
+
 	TEST_RX_BUFFER_init();
 	TEST_TX_BUFFER_init();
+
+	SIGNAL_TEST_DRIVER_START_RX_init();
+	SIGNAL_TEST_DRIVER_START_TX_init();
 }
 
 void test_driver_configure(TRX_DRIVER_CONFIGURATION* p_cfg) {
@@ -105,15 +117,20 @@ void test_driver_start_rx (u16 num_of_rx_bytes) {
 
 	TEST_RX_BUFFER_start_write();
 	test_driver_status_set(TEST_DRIVER_STATUS_RX_ACTIVE);
+
+	SIGNAL_TEST_DRIVER_START_RX_send((void*) &num_of_rx_bytes);
 }
 
 void test_driver_wait_for_rx(u8 num_bytes, u16 timeout_ms) {
+			
+	DEBUG_TRACE_byte(num_bytes, "test_driver_wait_for_rx() - Num byts to send: ");
+	DEBUG_TRACE_byte(TEST_RX_BUFFER_bytes_available(), "test_driver_wait_for_rx() - Num byts available: ");
 
-	rx_timer_start();
+	TRX_TIMER_start();
 
 	while (TEST_RX_BUFFER_bytes_available() < num_bytes) {
 
-		if (rx_timer_is_up(timeout_ms)) {
+		if (TRX_TIMER_is_up(timeout_ms)) {
 			DEBUG_PASS("test_driver_wait_for_rx() - Time is up");
 			break;
 		}
@@ -140,19 +157,27 @@ void test_driver_start_tx (void) {
 
 	test_driver_status_set(TEST_DRIVER_STATUS_TX_ACTIVE);
 	TEST_TX_BUFFER_start_read();
+
+	SIGNAL_TEST_DRIVER_START_TX_send((void*) &num_bytes);
 }
 
 void test_driver_wait_for_tx(u8 num_bytes, u16 timeout_ms) {
-	return;
-	tx_timer_start();
+			
+	DEBUG_TRACE_byte(num_bytes, "test_driver_wait_for_tx() - Num byts to send: ");
+	DEBUG_TRACE_byte(TEST_TX_BUFFER_bytes_available(), "test_driver_wait_for_tx() - Num byts available: ");
 
-	while (TEST_RX_BUFFER_bytes_available() != 0) {
+	TRX_TIMER_start();
 
-		if (tx_timer_is_up(timeout_ms)) {
-			DEBUG_PASS("test_driver_wait_for_tx() - Time is up");
-			break;
-		}
+	while (TRX_TIMER_is_up(timeout_ms) == 0) {
+
 	}
+
+	if (TEST_TX_BUFFER_bytes_available() != 0) {
+		DEBUG_PASS("test_driver_wait_for_tx() - still bytes left ...");
+		return;
+	}
+
+	test_driver_status_unset(TEST_DRIVER_STATUS_TX_ACTIVE);
 }
 
 void test_driver_stop_tx (void) {
@@ -174,6 +199,7 @@ void test_driver_clear_tx_buffer (void) {
 }
 
 void test_driver_set_address (u8 addr) {
+	DEBUG_TRACE_byte(addr, "test_driver_set_address()");
 	(void) addr;
 }
 
@@ -197,8 +223,6 @@ void test_driver_mutex_release(u8 m_id) {
 
 void test_driver_set_rx_bytes(u8 num_bytes, u8* p_buffer_from) {
 
-	DEBUG_PASS("test_driver_set_rx_bytes()");
-
 	if (num_bytes > TEST_RX_BUFFER_size()) {
 		num_bytes = TEST_RX_BUFFER_size();
 	}
@@ -211,6 +235,8 @@ void test_driver_set_rx_bytes(u8 num_bytes, u8* p_buffer_from) {
 }
 
 u8 test_driver_get_tx_bytes(u8 num_bytes, u8* p_buffer_to) {
+
+	DEBUG_TRACE_byte(num_bytes, "test_driver_clear_rx_buffer()");
 
 	TEST_TX_BUFFER_start_read();
 	u16 num_bytes_read = TEST_TX_BUFFER_get_N_bytes(num_bytes, p_buffer_to);
@@ -226,5 +252,7 @@ u8 test_driver_get_tx_bytes(u8 num_bytes, u8* p_buffer_to) {
 }
 
 u8 test_driver_get_tx_bytes_num_available(void) {
+
+	DEBUG_TRACE_byte(TEST_TX_BUFFER_bytes_available(), "test_driver_get_tx_bytes_num_available()");
 	return TEST_TX_BUFFER_bytes_available();
 }
