99dfa36f0efb75cac6eb66ad78cb9a2afd45ccdf
smuuze <sebastia_n@web.de>
2020-04-02 08:26:09 +0000

    FIX: spi0_driver_atmega1284p - incorrect check for timeout on transmit

--- src/driver/communication/spi/spi0_driver_atmega1284p.c
+++ src/driver/communication/spi/spi0_driver_atmega1284p.c
@@ -487,7 +487,7 @@ void spi_driver_wait_for_tx(u8 num_bytes, u16 timeout_ms) {
 
 	while (bytes_transmitted < num_bytes) {
 
-		if (time_mgmnt_istimeup_u16(time_reference_ms, timeout_ms) == 0) {
+		if (time_mgmnt_istimeup_u16(time_reference_ms, timeout_ms)) {
 			return;
 		}
 
