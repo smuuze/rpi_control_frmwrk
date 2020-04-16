1f12742fd418e2ab863b6df1fd8c3f7310a4ad35
smuuze <sebastia_n@web.de>
2020-04-16 05:29:52 +0000

    NEW: rpi_command_handler_table - table is now located in a seperate file

---
+++ src/command_handler/rpi_command_handler_table.c
@@ -0,0 +1,58 @@
+
+ /*
+  * \@file	command_handler/rpi_command_handler_table.c
+  * \author	sebastian lesse
+  */
+
+#define TRACER_ON
+
+//-----------------------------------------------------------------------------
+
+#include "config.h"  // immer als erstes einbinden!
+
+//-----------------------------------------------------------------------------
+
+#include "tracer.h"
+
+//-----------------------------------------------------------------------------
+
+#include "command_management/command_handler_interface.h"
+#include "command_management/command_buffer_interface.h"
+#include "command_management/answer_buffer_interface.h"
+#include "command_management/protocol_interface.h"
+
+#include "io_management/io_input_controller.h"
+#include "io_management/io_output_controller.h"
+
+#include "command_handler/rpi_command_handler.h"
+
+//-----------------------------------------------------------------------------
+
+COMMAND_TABLE_INTERFACE rpi_cmd_handler_table[] = {
+	{RPI_COMMAND_GET_VERSION , &rpi_cmd_get_version},
+
+	#ifdef RPI_CMD_HANDLER_IO_AVAILABLE
+	{RPI_COMMAND_GET_INPUT_LIST, &rpi_cmd_get_input_list},
+	{RPI_COMMAND_GET_OUTPUT_LIST, &rpi_cmd_get_output_list},
+	{RPI_COMMAND_SET_OUTPUT, &rpi_cmd_set_output},
+	{RPI_COMMAND_GET_INPUT_STATE, &rpi_cmd_get_input_state},
+	{RPI_COMMAND_GET_OUTPUT_STATE, &rpi_cmd_get_output_state},
+	#endif
+
+	#ifdef RPI_CMD_HANDLER_SENSOR_AVAILABLE
+	{RPI_COMMAND_GET_TEMPERATURE, &rpi_cmd_get_temperature},
+	{RPI_COMMAND_GET_HUMIDTY, &rpi_cmd_get_humidity},
+	{RPI_COMMAND_GET_ADC, &rpi_cmd_get_adc},
+	{RPI_COMMAND_GET_LIGHT, &rpi_cmd_get_light}
+	#endif
+
+	#ifdef RPI_CMD_HANDLER_IR_REMOTE_AVAILABLE
+	{RPI_COMMAND_IR_REMOTE, &rpi_cmd_handler_ir_remote},
+	#endif
+};
+
+//-----------------------------------------------------------------------------
+
+u8 rpi_cmd_handler_table_get_size(void) {
+	return (sizeof(rpi_cmd_handler_table) / sizeof(COMMAND_TABLE_INTERFACE));
+}
