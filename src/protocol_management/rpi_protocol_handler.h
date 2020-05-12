4a72ee7b376ca1f76ca4cd92ff977619dc7bfccf
smuuze <sebastia_n@web.de>
2020-05-12 06:18:55 +0100

    ADD: rpi_protocol_handler - new definitions for i2c driver configuration

--- src/protocol_management/rpi_protocol_handler.h
+++ src/protocol_management/rpi_protocol_handler.h
@@ -1,25 +1,44 @@
-/*! \file *********************************************************************
-
- *****************************************************************************/
+ /*
+  * \@file	protocol_management/rpi_protocl_handler.h
+  * \author	sebastian lesse
+  */
 
 #ifndef RPI_PROTOCOL_HANDLER_H_
 #define RPI_PROTOCOL_HANDLER_H_
 
+//-----------------------------------------------------------------------------
+
 #include "config.h"  // immer als erstes einbinden!
-#include "specific.h"
+
+//-----------------------------------------------------------------------------
 
 #include "driver/trx_driver_interface.h"
 #include "driver/cfg_driver_interface.h"
 #include "mcu_task_management/mcu_task_interface.h"
 
+//-----------------------------------------------------------------------------
+
 #ifndef RPI_PROTOCOL_HANDLER_DRIVER_CFG
-#define RPI_PROTOCOL_HANDLER_DRIVER_CFG			DRIVER_SPI_SLAVE, 		/*	u8 op_mode; 		*/ \
+#define RPI_PROTOCOL_HANDLER_DRIVER_CFG			COM_DRIVER_IS_SLAVE, 		/*	u8 op_mode; 		*/ \
 							DRIVER_SPI_MODE_3, 		/*	u8 clk_phase;		*/ \
 							DRIVER_SPI_CLK_DEVIDER_4, 	/*	u8 clk_divider;		*/ \
 							DRIVER_SPI_NO_DOUBLE_SPEED, 	/*	u8 clk_double_speed;	*/ \
 							DRIVER_SPI_DATA_ORDER_MSB, 	/*	u8 data_order;		*/ \
-							DRIVER_SPI_INTERRUPT_DISABLE	/*	u8 interrupt_enable;	*/
+							COM_DRIVER_IRQ_DISABLE	/*	u8 interrupt_enable;	*/
 #endif
+
+//-----------------------------------------------------------------------------
+
+#ifndef RPI_PROTOCOL_I2C_HANDLER_DRIVER_CFG
+#define RPI_PROTOCOL_I2C_HANDLER_DRIVER_CFG		COM_DRIVER_IS_SLAVE, 		/*	u8 op_mode; 		*/ \
+							DRIVER_SPI_MODE_3, 		/*	u8 clk_phase;		*/ \
+							DRIVER_SPI_CLK_DEVIDER_4, 	/*	u8 clk_divider;		*/ \
+							DRIVER_SPI_NO_DOUBLE_SPEED, 	/*	u8 clk_double_speed;	*/ \
+							DRIVER_SPI_DATA_ORDER_MSB, 	/*	u8 data_order;		*/ \
+							COM_DRIVER_IRQ_DISABLE		/*	u8 interrupt_enable;	*/
+#endif
+
+//-----------------------------------------------------------------------------
 
 /*!
  *
@@ -27,6 +46,8 @@
 #ifndef RPI_PROTOCOL_COMMAND_READ_TIMEOUT_MS
 #define RPI_PROTOCOL_COMMAND_READ_TIMEOUT_MS		1000
 #endif
+
+//-----------------------------------------------------------------------------
 
 /*!
  *
@@ -43,6 +64,8 @@ void rpi_protocol_com_driver_rx_complete_callback(void);
  *
  */
 void rpi_protocol_com_driver_tx_complete_callback(void);
+
+//-----------------------------------------------------------------------------
 
 /*!
  *
@@ -64,11 +87,6 @@ MCU_TASK_INTERFACE_TASK_STATE rpi_protocol_task_get_state(void);
  *
  */
 void rpi_protocol_task_run(void);
-
-/*!
- *
- */
-void rpi_protocol_handler_debus_handler(void);
 
 /*!
  *
