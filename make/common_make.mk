b3c87913fdc94e7d721a3a0219d690db315461be
smuuze <sebastia_n@web.de>
2020-12-21 14:16:29 +0100

    UPDATE: common_make to use new module command_tools_datetime

--- make/common_make.mk
+++ make/common_make.mk
@@ -55,6 +55,7 @@ CSRCS += $(COMMON_INC_PATH)/local_msg_buffer.c
 CSRCS += $(COMMON_INC_PATH)/signal_slot_interface.c
 CSRCS += $(COMMON_INC_PATH)/math_module.c
 CSRCS += $(COMMON_INC_PATH)/common_tools_string.c
+CSRCS += $(COMMON_INC_PATH)/common_tools_datetime.c
 
 ifneq '' '$(findstring QEUE,$(COMMON_MODULES))'
 DEFS  += -D HAS_QEUE_INTERFACE=1
