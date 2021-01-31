9a9dc790c4660bde55052ea0379cdb549098cd36
smuuze <sebastia_n@web.de>
2021-01-31 12:35:35 +0100

    ADD: rpi_protocol_handler - spi-default speed set to 19200

--- src/protocol_management/rpi_protocol_handler.h
+++ src/protocol_management/rpi_protocol_handler.h
@@ -24,13 +24,20 @@
 
 //-----------------------------------------------------------------------------
 
+#ifndef RPI_PROTOCOL_HANDLER_DEFAULT_SPI_SPEED_HZ
+#define RPI_PROTOCOL_HANDLER_DEFAULT_SPI_SPEED_HZ
+#endif
+
+//-----------------------------------------------------------------------------
+
 #ifndef RPI_PROTOCOL_HANDLER_DRIVER_CFG
 #define RPI_PROTOCOL_HANDLER_DRIVER_CFG			.is_master = COM_DRIVER_IS_SLAVE, 			/*	u8 op_mode; 		*/ \
 							.mode = DRIVER_SPI_MODE_3, 				/*	u8 clk_phase;		*/ \
 							.clk_divider = DRIVER_SPI_CLK_DEVIDER_4, 		/*	u8 clk_divider;		*/ \
 							.clk_double_speed = DRIVER_SPI_NO_DOUBLE_SPEED, 	/*	u8 clk_double_speed;	*/ \
 							.data_order = DRIVER_SPI_DATA_ORDER_MSB, 		/*	u8 data_order;		*/ \
-							.interrupt_enable = COM_DRIVER_IRQ_ENABLE		/*	u8 interrupt_enable;	*/
+							.interrupt_enable = COM_DRIVER_IRQ_ENABLE,		/*	u8 interrupt_enable;	*/ \
+							.speed = DRIVER_SPI_SPEED_HZ_19200			/*	u32 speed;		*/
 #endif
 
 //-----------------------------------------------------------------------------
