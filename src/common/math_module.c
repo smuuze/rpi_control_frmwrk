d120d6cc6e654b3ccd47b6f07ae1ded6ac149c79
smuuze <sebastia_n@web.de>
2021-02-07 16:12:16 +0100

    FIX: math_module - Added function for signed division

--- src/common/math_module.c
+++ src/common/math_module.c
@@ -1,13 +1,31 @@
+/**
+ * @file math_module.h
+ * @author sebastian lesse (sebastian lesse)
+ * @brief 
+ * @version 1.0
+ * @date 2021-02-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #define TRACER_ON
 
+// --------------------------------------------------------------------------------
 #include "config.h"
+
+// --------------------------------------------------------------------------------
+
 #include "tracer.h"
 
-/*!
- *
- */
+// --------------------------------------------------------------------------------
+
 u32 math_div_u32(u32 dividend, u32 divisor) {
+
+	if (divisor == 0) {
+		return 0;
+	}
+
 	u32 result = 0;
 
 	while (dividend >= divisor) {
@@ -16,4 +34,31 @@ u32 math_div_u32(u32 dividend, u32 divisor) {
 	}
 
 	return result;
+}
+
+i32 math_div_i32(i32 dividend, i32 divisor) {
+
+	u8 dividend_is_negative = (dividend < 0) ? 1 : 0;
+	u8 divisor_is_negative = (divisor < 0) ? 1 : 0;
+
+	if (dividend_is_negative && divisor_is_negative) {
+
+		// both values are negative, so result is also positive
+		return (i32)math_div_u32((u32)(dividend * (-1)), (u32)(divisor * (-1)));
+
+	} if (dividend_is_negative) {
+
+		// only dividend is negative, so result is negative
+		return (i32)math_div_u32((u32)(dividend * (-1)), (u32)(divisor)) * (-1);
+
+	} if (divisor_is_negative) {
+
+		// only divisor is negative, so result is negative
+		return (i32)math_div_u32((u32)(dividend), (u32)(divisor * (-1))) * (-1);
+
+	}  else {
+
+		// both values are positive, so result is also positive
+		return (i32)math_div_u32((u32)dividend, (u32)divisor);
+	}
 }
--- src/common/math_module.h
+++ src/common/math_module.h
@@ -1,8 +1,53 @@
 #ifndef _MATH_MODULE_H_
 #define _MATH_MODULE_H_
 
+/**
+ * @file math_module.h
+ * @author sebastian lesse (sebastian lesse)
+ * @brief 
+ * @version 1.0
+ * @date 2021-02-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+// --------------------------------------------------------------------------------
+
 #include "config.h"
 
+// --------------------------------------------------------------------------------
+
+/**
+ * @brief 	Performs a unsigned devision
+ * 		If the divisor is equal to 0, this function returns 0.
+ * 
+ * @param dividend 
+ * @param divisor 
+ * @return u32 
+ */
 u32 math_div_u32(u32 dividend, u32 divisor);
+
+/**
+ * @brief 	Performs a signed division
+ * 		If the divisor is equal to 0, this function returns 0.
+ * 
+ * @param dividend 
+ * @param divisor 
+ * @return i32 
+ */
+i32 math_div_i32(u32 dividend, u32 divisor);
+
+// --------------------------------------------------------------------------------
+
+#define div_u8(divident, divisor)		(u8) math_div_u32((u32)divident, (u32) divisor)
+#define div_u16(divident, divisor)		(u16)math_div_u32((u32)divident, (u32) divisor)
+#define div_u32(divident, divisor)		(u32)math_div_u32((u32)divident, (u32) divisor)
+
+#define div_i8(divident, divisor)		(i8) math_div_i32((i32)divident, (i32) divisor)
+#define div_i16(divident, divisor)		(u16)math_div_u32((i32)divident, (i32) divisor)
+#define div_i32(divident, divisor)		(i32)math_div_u32((i32)divident, (i32) divisor)
+
+// --------------------------------------------------------------------------------
 
 #endif // _MATH_MODULE_H_
