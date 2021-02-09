c97546193be3ca4a44cc24488c5edfe580853fc2
smuuze <sebastia_n@web.de>
2021-02-09 17:31:41 +0100

    ADD: common_tools_number - new module to parse/convert numbers

---
+++ src/common/common_tools_number.c
@@ -0,0 +1,83 @@
+/**
+ * @file 	common_tools_number.c
+ * @author 	sebastian lesse (sebastian lesse)
+ * @brief 	Converts a number to a byte-array and vice versa
+ * @version 	1.0
+ * @date 	2021-02-08
+ * 
+ * @copyright 	Copyright (c) 2021
+ * 
+ */
+
+#define TRACER_OFF
+
+// --------------------------------------------------------------------------------
+
+#include "config.h"
+
+// --------------------------------------------------------------------------------
+
+#include "tracer.h"
+
+// --------------------------------------------------------------------------------
+
+u32 common_tools_number_u32_from_array(const u8* p_array) {
+
+	u32 number  = (u32)p_array[3] << 24;
+	    number += (u32)p_array[2] << 16;
+	    number += (u32)p_array[1] << 8;
+	    number += (u32)p_array[0] << 0;
+
+	return number;
+}
+
+u16 common_tools_number_u16_from_array(const u8* p_array) {
+
+	u16 number  = (u16)p_array[1] << 8;
+	    number += (u16)p_array[0] << 0;
+
+	return number;
+}
+
+i32 common_tools_number_i32_from_array(const u8* p_array) {
+
+	i32 number  = (i32)p_array[3] << 24;
+	    number += (i32)p_array[2] << 16;
+	    number += (i32)p_array[1] << 8;
+	    number += (i32)p_array[0] << 0;
+
+	return number;
+}
+
+i16 common_tools_number_i16_from_array(const u8* p_array) {
+
+	i16 number  = p_array[1] << 8;
+	    number += p_array[0] << 0;
+
+	return number;
+}
+
+void common_tools_number_u32_to_array(u8* p_array, u32 number) {
+	
+	p_array[3] = (u8)(number >> 24);
+	p_array[2] = (u8)(number >> 16);
+	p_array[1] = (u8)(number >> 8);
+	p_array[0] = (u8)(number >> 0);
+}
+
+void common_tools_number_u16_to_array(u8* p_array, u16 number) {
+	
+	p_array[1] = (u8)(number >> 8);
+	p_array[0] = (u8)(number >> 0);
+}
+
+i32 common_tools_number_from_u8(u8 number, u8 is_signed) {
+
+	if (is_signed) {
+		return (i32)(i8)number;
+	} else {
+		return (i32)number;
+	}
+}
+
+// --------------------------------------------------------------------------------
---
+++ src/common/common_tools_number.h
@@ -0,0 +1,80 @@
+#ifndef COMMON_TOOLS_NUMBER_H_
+#define COMMON_TOOLS_NUMBER_H_
+
+/**
+ * @file 	common_tools_number.h
+ * @author 	sebastian lesse (sebastian lesse)
+ * @brief 	Converts a number to a byte-array and vice versa
+ * @version 	1.0
+ * @date 	2021-02-08
+ * 
+ * @copyright 	Copyright (c) 2021
+ * 
+ */
+
+// --------------------------------------------------------------------------------
+
+#include "config.h"
+
+// --------------------------------------------------------------------------------
+
+/**
+ * @brief Converts the byte-array to a unsigned 32 bit value in LSByte first order
+ * 
+ * @param p_array byte-array 
+ * @return number in LSByte first order
+ */
+u32 common_tools_number_u32_from_array(const u8* p_array);
+
+/**
+ * @brief Converts the byte-array to a unsigned 16 bit value in LSByte first order
+ * 
+ * @param p_array byte-array 
+ * @return number in LSByte first order
+ */
+u16 common_tools_number_u16_from_array(const u8* p_array);
+
+/**
+ * @brief Converts the byte-array to a signed 32 bit value in LSByte first order
+ * 
+ * @param p_array byte-array 
+ * @return number in LSByte first order
+ */
+i32 common_tools_number_i32_from_array(const u8* p_array);
+
+/**
+ * @brief Converts the byte-array to a signed 16 bit value in LSByte first order
+ * 
+ * @param p_array byte-array 
+ * @return number in LSByte first order
+ */
+i16 common_tools_number_i16_from_array(const u8* p_array);
+
+/**
+ * @brief Copies a 32 bit number into a byte-array in LSByte first order
+ * 
+ * @param p_array byte array where the number is copied into
+ * @param number 32 bit number
+ */
+void common_tools_number_u32_to_array(u8* p_array, u32 number);
+
+/**
+ * @brief Copies a 16 bit number into a byte-array in LSByte first order
+ * 
+ * @param p_array byte array where the number is copied into
+ * @param number 16 bit number
+ */
+void common_tools_number_u16_to_array(u8* p_array, u16 number);
+
+/**
+ * @brief Converts a u8 value into a i32 value by respecting the signess of the u8 value
+ * 
+ * @param number u8 number, can be signed or unsigned
+ * @param is_signed if 1 the signess of the number is respected, 0 signess is not respected
+ * @return signed 32 bit value
+ */
+i32 common_tools_number_from_u8(u8 number, u8 is_signed);
+
+// --------------------------------------------------------------------------------
+
+#endif //COMMON_TOOLS_NUMBER_H_
