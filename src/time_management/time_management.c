047b700624a3b7f67f67e52d0785dc1f775e6f4a
smuuze <sebastia_n@web.de>
2021-02-20 09:29:13 +0100

    FIX: time_management - interface now uses context-object to create timer

--- src/time_management/time_management.c
+++ src/time_management/time_management.c
@@ -1,7 +1,7 @@
 /*! 
  * --------------------------------------------------------------------------------
  *
- * 	@file		main_rpi_hat.c
+ * 	@file		time_management.c
  * 	@author		sebastian lesse
  *
  * --------------------------------------------------------------------------------
@@ -36,26 +36,139 @@ u32 time_mgmnt_gettime_u32(void) {
 	return local_rtc_timer_gettime_u32();
 }
 
-u8 time_mgmnt_istimeup_u8(u8 time_reference, u8 time_interval) {
-	return local_rtc_timer_istimeup_u8(time_reference, time_interval);
+// --------------------------------------------------------------------------------
+
+void time_mgmnt_start_u8(TIME_MANAGEMENT_U8_TYPE* p_context) {
+	p_context->time_reference = local_rtc_timer_gettime_u8();
+	p_context->status = TIME_MGMNT_STATUS_ACTIVE;
 }
 
-u8 time_mgmnt_istimeup_u16(u16 time_reference, u16 time_interval) {
-	return local_rtc_timer_istimeup_u16(time_reference, time_interval);
+void time_mgmnt_start_u16(TIME_MANAGEMENT_U16_TYPE* p_context) {
+	p_context->time_reference = local_rtc_timer_gettime_u16();
+	p_context->status = TIME_MGMNT_STATUS_ACTIVE;
 }
 
-u8 time_mgmnt_istimeup_u32(u32 time_reference, u32 time_interval) {
-	return local_rtc_timer_istimeup_u32(time_reference, time_interval);
+void time_mgmnt_start_u32(TIME_MANAGEMENT_U32_TYPE* p_context) {
+	p_context->time_reference = local_rtc_timer_gettime_u32();
+	p_context->status = TIME_MGMNT_STATUS_ACTIVE;
 }
 
-u8 time_mgmnt_elapsed_u8(u8 time_reference) {
-	return rtc_timer_elapsed_u8(time_reference);
+// --------------------------------------------------------------------------------
+
+void time_mgmnt_stop_u8(TIME_MANAGEMENT_U8_TYPE* p_context) {
+	p_context->time_reference = 0;
+	p_context->status = 0;
 }
 
-u16 time_mgmnt_elapsed_u16(u16 time_reference) {
-	return rtc_timer_elapsed_u16(time_reference);
+void time_mgmnt_stop_u16(TIME_MANAGEMENT_U16_TYPE* p_context) {
+	p_context->time_reference = 0;
+	p_context->status = 0;
 }
 
-u32 time_mgmnt_elapsed_u32(u32 time_reference) {
-	return rtc_timer_elapsed_u32(time_reference);
+void time_mgmnt_stop_u32(TIME_MANAGEMENT_U32_TYPE* p_context) {
+	p_context->time_reference = 0;
+	p_context->status = 0;
+}
+
+// --------------------------------------------------------------------------------
+
+u8 time_mgmnt_is_active_u8(TIME_MANAGEMENT_U8_TYPE* p_context) {
+	return p_context->status == TIME_MGMNT_STATUS_ACTIVE ? 1 : 0;
+}
+
+u8 time_mgmnt_is_active_u16(TIME_MANAGEMENT_U16_TYPE* p_context) {
+	return p_context->status == TIME_MGMNT_STATUS_ACTIVE ? 1 : 0;
+}
+
+u8 time_mgmnt_is_active_u32(TIME_MANAGEMENT_U32_TYPE* p_context) {
+	return p_context->status == TIME_MGMNT_STATUS_ACTIVE ? 1 : 0;
+}
+
+// --------------------------------------------------------------------------------
+
+u8 time_mgmnt_istimeup_u8(TIME_MANAGEMENT_U8_TYPE* p_context, u8 time_interval) {
+	return (time_mgmnt_elapsed_u8(p_context) >= time_interval) ? 1 : 0;
+}
+
+u8 time_mgmnt_istimeup_u16(TIME_MANAGEMENT_U16_TYPE* p_context, u16 time_interval) {
+	return time_mgmnt_elapsed_u16(p_context) >= time_interval ? 1 : 0;
+}
+
+u8 time_mgmnt_istimeup_u32(TIME_MANAGEMENT_U32_TYPE* p_context, u32 time_interval) {
+	return time_mgmnt_elapsed_u32(p_context) >= time_interval ? 1 : 0;
+}
+
+// --------------------------------------------------------------------------------
+
+u8 time_mgmnt_istimeup_raw_u8(u8 time_reference, u8 time_interval) {
+
+	TIME_MANAGEMENT_U8_TYPE context = {
+		.status = TIME_MGMNT_STATUS_ACTIVE,
+		.time_reference = time_reference
+	};
+
+	return time_mgmnt_istimeup_u8(&context, time_interval);
+}
+
+u8 time_mgmnt_istimeup_raw_u16(u16 time_reference, u16 time_interval) {
+	
+	TIME_MANAGEMENT_U16_TYPE context = {
+		.status = TIME_MGMNT_STATUS_ACTIVE,
+		.time_reference = time_reference
+	};
+
+	return time_mgmnt_istimeup_u16(&context, time_interval);
+}
+
+u8 time_mgmnt_istimeup_raw_u32(u32 time_reference, u32 time_interval) {
+	
+	TIME_MANAGEMENT_U32_TYPE context = {
+		.status = TIME_MGMNT_STATUS_ACTIVE,
+		.time_reference = time_reference
+	};
+
+	return time_mgmnt_istimeup_u32(&context, time_interval);
+}
+
+// --------------------------------------------------------------------------------
+
+u8 time_mgmnt_elapsed_u8(TIME_MANAGEMENT_U8_TYPE* p_context) {
+
+	if (p_context->status != TIME_MGMNT_STATUS_ACTIVE) {
+		return 0;
+	}
+
+	return local_rtc_timer_gettime_u8() - p_context->time_reference;
+}
+
+u16 time_mgmnt_elapsed_u16(TIME_MANAGEMENT_U16_TYPE* p_context) {
+
+	if (p_context->status != TIME_MGMNT_STATUS_ACTIVE) {
+		return 0;
+	}
+
+	return local_rtc_timer_gettime_u16() - p_context->time_reference;
+}
+
+u32 time_mgmnt_elapsed_u32(TIME_MANAGEMENT_U32_TYPE* p_context) {
+
+	if (p_context->status != TIME_MGMNT_STATUS_ACTIVE) {
+		return 0;
+	}
+
+	return local_rtc_timer_gettime_u32() - p_context->time_reference;
+}
+
+// --------------------------------------------------------------------------------
+
+u8 time_mgmnt_start_time_u8(TIME_MANAGEMENT_U8_TYPE* p_context) {
+	return p_context->time_reference;
+}
+
+u16 time_mgmnt_start_time_u16(TIME_MANAGEMENT_U16_TYPE* p_context) {
+	return p_context->time_reference;
+}
+
+u32 time_mgmnt_start_time_u32(TIME_MANAGEMENT_U32_TYPE* p_context) {
+	return p_context->time_reference;
 }
--- src/time_management/time_management.h
+++ src/time_management/time_management.h
@@ -1,164 +1,348 @@
+/*! 
+ * --------------------------------------------------------------------------------
+ *
+ * 	@file		time_management.h
+ * 	@author		sebastian lesse
+ *
+ * --------------------------------------------------------------------------------
+ */
+
 #ifndef _TIME_MANAGEMENT_H_
 #define _TIME_MANAGEMENT_H_
 
+// --------------------------------------------------------------------------------
+
 #include "config.h"
+
+// --------------------------------------------------------------------------------
+
+/**
+ * @brief 
+ * 
+ */
+typedef struct {
+	u8 status;
+	u8 time_reference;
+} TIME_MANAGEMENT_U8_TYPE;
+
+/**
+ * @brief 
+ * 
+ */
+typedef struct {
+	u8 status;
+	u16 time_reference;
+} TIME_MANAGEMENT_U16_TYPE;
+
+/**
+ * @brief 
+ * 
+ */
+typedef struct {
+	u8 status;
+	u32 time_reference;
+} TIME_MANAGEMENT_U32_TYPE;
+
+// --------------------------------------------------------------------------------
+
+#define TIME_MGMNT_STATUS_ACTIVE					(1 << 0)
+
+// --------------------------------------------------------------------------------
 
 #define TIME_MGMN_BUILD_STATIC_TIMER_U8(name)									\
 														\
-	static u8 _##name##_time_reference = 0;									\
-	static u8 _##name##_timer_status = 0;									\
+	static TIME_MANAGEMENT_U8_TYPE _##name##_time_mgmnt_context;						\
 														\
 	__UNUSED__ static inline void name##_start(void) {							\
-		_##name##_time_reference = time_mgmnt_gettime_u8();						\
-		_##name##_timer_status = 1;									\
+		time_mgmnt_start_u8(&_##name##_time_mgmnt_context);						\
 	}													\
 														\
 	__UNUSED__ static inline void name##_stop(void) {							\
-		_##name##_timer_status = 0;									\
+		time_mgmnt_stop_u8(&_##name##_time_mgmnt_context);						\
 	}													\
 														\
 	__UNUSED__ static inline u8 name##_is_up(u8 time_interval) {						\
-		return time_mgmnt_istimeup_u8(_##name##_time_reference, time_interval);				\
+		return time_mgmnt_istimeup_u8(&_##name##_time_mgmnt_context, time_interval);			\
 	}													\
 														\
 	__UNUSED__ static inline u8 name##_start_time(void) {							\
-		return _##name##_time_reference;								\
+		return time_mgmnt_start_time_u8(&_##name##_time_mgmnt_context);					\
 	}													\
 														\
 	__UNUSED__ static inline u8 name##_is_active(void) {							\
-		return _##name##_timer_status != 0 ? 1 : 0;							\
+		return time_mgmnt_is_active_u8(&_##name##_time_mgmnt_context);					\
 	}													\
 														\
 	__UNUSED__ static inline u8 name##_elapsed(void) {							\
-		return time_mgmnt_elapsed_u8(_##name##_time_reference);						\
+		return time_mgmnt_elapsed_u8(&_##name##_time_mgmnt_context);					\
 	}
 
 #define TIME_MGMN_BUILD_STATIC_TIMER_U16(name)									\
 														\
-	static u16 _##name##_time_reference = 0;								\
-	static u8 _##name##_timer_status = 0;									\
+	static TIME_MANAGEMENT_U16_TYPE _##name##_time_mgmnt_context;						\
 														\
 	__UNUSED__ static inline void name##_start(void) {							\
-		_##name##_time_reference = time_mgmnt_gettime_u16();						\
-		_##name##_timer_status = 1;									\
+		time_mgmnt_start_u16(&_##name##_time_mgmnt_context);						\
 	}													\
 														\
 	__UNUSED__ static inline void name##_stop(void) {							\
-		_##name##_timer_status = 0;									\
+		time_mgmnt_stop_u16(&_##name##_time_mgmnt_context);						\
 	}													\
 														\
 	__UNUSED__ static inline u8 name##_is_up(u16 time_interval) {						\
-		return time_mgmnt_istimeup_u16(_##name##_time_reference, time_interval);			\
+		return time_mgmnt_istimeup_u16(&_##name##_time_mgmnt_context, time_interval);			\
 	}													\
 														\
 	__UNUSED__ static inline u16 name##_start_time(void) {							\
-		return _##name##_time_reference;								\
+		return time_mgmnt_start_time_u16(&_##name##_time_mgmnt_context);				\
 	}													\
 														\
 	__UNUSED__ static inline u8 name##_is_active(void) {							\
-		return _##name##_timer_status != 0 ? 1 : 0;							\
+		return time_mgmnt_is_active_u16(&_##name##_time_mgmnt_context);					\
 	}													\
 														\
 	__UNUSED__ static inline u16 name##_elapsed(void) {							\
-		return time_mgmnt_elapsed_u16(_##name##_time_reference);					\
+		return time_mgmnt_elapsed_u16(&_##name##_time_mgmnt_context);					\
 	}
 
 #define TIME_MGMN_BUILD_STATIC_TIMER_U32(name)									\
 														\
-	static u32 _##name##_time_reference = 0;								\
-	static u8 _##name##_timer_status = 0;									\
+	static TIME_MANAGEMENT_U32_TYPE _##name##_time_mgmnt_context;						\
 														\
 	__UNUSED__ static inline void name##_start(void) {							\
-		_##name##_time_reference = time_mgmnt_gettime_u32();						\
-		_##name##_timer_status = 1;									\
+		time_mgmnt_start_u32(&_##name##_time_mgmnt_context);						\
 	}													\
 														\
 	__UNUSED__ static inline void name##_stop(void) {							\
-		_##name##_timer_status = 0;									\
+		time_mgmnt_stop_u32(&_##name##_time_mgmnt_context);						\
 	}													\
 														\
 	__UNUSED__ static inline u8 name##_is_up(u32 time_interval) {						\
-		return time_mgmnt_istimeup_u32(_##name##_time_reference, time_interval);			\
+		return time_mgmnt_istimeup_u32(&_##name##_time_mgmnt_context, time_interval);			\
 	}													\
 														\
 	__UNUSED__ static inline u32 name##_start_time(void) {							\
-		return _##name##_time_reference;								\
+		return time_mgmnt_start_time_u32(&_##name##_time_mgmnt_context);				\
 	}													\
 														\
 	__UNUSED__ static inline u8 name##_is_active(void) {							\
-		return _##name##_timer_status != 0 ? 1 : 0;							\
+		return time_mgmnt_is_active_u32(&_##name##_time_mgmnt_context);					\
 	}													\
 														\
 	__UNUSED__ static inline u32 name##_elapsed(void) {							\
-		return time_mgmnt_elapsed_u32(_##name##_time_reference);					\
+		return time_mgmnt_elapsed_u32(&_##name##_time_mgmnt_context);					\
 	}
+
+
+// --------------------------------------------------------------------------------
 
 #define TIME_MGMNT_MAX_TIME_U8							0xFF
 #define TIME_MGMNT_MAX_TIME_U16							0xFFFF
 #define TIME_MGMNT_MAX_TIME_U32							0xFFFFFFFF
 
+// --------------------------------------------------------------------------------
 
-/*!
- *
- * @return
+/**
+ * @brief 
+ * 
+ * @return u8 
  */
 u8 time_mgmnt_gettime_u8(void);
 
-/*!
- *
- * @return
+/**
+ * @brief 
+ * 
+ * @return u16 
  */
 u16 time_mgmnt_gettime_u16(void);
 
-/*!
- *
- * @return
+/**
+ * @brief 
+ * 
+ * @return u32 
  */
 u32 time_mgmnt_gettime_u32(void);
 
-/*!
- *
- * @param time_reference
- * @param time_interval
- * @return
- */
-u8 time_mgmnt_istimeup_u8(u8 time_reference, u8 time_interval);
+// --------------------------------------------------------------------------------
 
-/*!
- *
- * @param time_reference
- * @param time_interval
- * @return
+/**
+ * @brief 
+ * 
+ * @param p_context 
  */
-u8 time_mgmnt_istimeup_u16(u16 time_reference, u16 time_interval);
+void time_mgmnt_start_u8(TIME_MANAGEMENT_U8_TYPE* p_context);
 
-/*!
- *
- * @param time_reference
- * @param time_interval
- * @return
+/**
+ * @brief 
+ * 
+ * @param p_context 
  */
-u8 time_mgmnt_istimeup_u32(u32 time_reference, u32 time_interval);
+void time_mgmnt_start_u16(TIME_MANAGEMENT_U16_TYPE* p_context);
 
-/*!
- *
- * @param time_reference
- * @return
+/**
+ * @brief 
+ * 
+ * @param p_context 
  */
-u8 time_mgmnt_elapsed_u8(u8 time_reference);
+void time_mgmnt_start_u32(TIME_MANAGEMENT_U32_TYPE* p_context);
 
-/*!
- *
- * @param time_reference
- * @return
- */
-u16 time_mgmnt_elapsed_u16(u16 time_reference);
+// --------------------------------------------------------------------------------
 
-/*!
- *
- * @param time_reference
- * @return
+/**
+ * @brief 
+ * 
+ * @param p_context 
  */
-u32 time_mgmnt_elapsed_u32(u32 time_reference);
+void time_mgmnt_stop_u8(TIME_MANAGEMENT_U8_TYPE* p_context);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ */
+void time_mgmnt_stop_u16(TIME_MANAGEMENT_U16_TYPE* p_context);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ */
+void time_mgmnt_stop_u32(TIME_MANAGEMENT_U32_TYPE* p_context);
+
+// --------------------------------------------------------------------------------
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u8 
+ */
+u8 time_mgmnt_is_active_u8(TIME_MANAGEMENT_U8_TYPE* p_context);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u8 
+ */
+u8 time_mgmnt_is_active_u16(TIME_MANAGEMENT_U16_TYPE* p_context);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u8 
+ */
+u8 time_mgmnt_is_active_u32(TIME_MANAGEMENT_U32_TYPE* p_context);
+
+// --------------------------------------------------------------------------------
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @param time_interval 
+ * @return u8 
+ */
+u8 time_mgmnt_istimeup_u8(TIME_MANAGEMENT_U8_TYPE* p_context, u8 time_interval);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @param time_interval 
+ * @return u8 
+ */
+u8 time_mgmnt_istimeup_u16(TIME_MANAGEMENT_U16_TYPE* p_context, u16 time_interval);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @param time_interval 
+ * @return u8 
+ */
+u8 time_mgmnt_istimeup_u32(TIME_MANAGEMENT_U32_TYPE* p_context, u32 time_interval);
+
+// --------------------------------------------------------------------------------
+
+/**
+ * @brief 
+ * 
+ * @param time_reference 
+ * @param time_interval 
+ * @return u8 
+ */
+u8 time_mgmnt_istimeup_raw_u8(u8 time_reference, u8 time_interval);
+
+/**
+ * @brief 
+ * 
+ * @param time_reference 
+ * @param time_interval 
+ * @return u8 
+ */
+u8 time_mgmnt_istimeup_raw_u16(u16 time_reference, u16 time_interval);
+
+/**
+ * @brief 
+ * 
+ * @param time_reference 
+ * @param time_interval 
+ * @return u8 
+ */
+u8 time_mgmnt_istimeup_raw_u32(u32 time_reference, u32 time_interval);
+
+// --------------------------------------------------------------------------------
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u8 
+ */
+u8 time_mgmnt_elapsed_u8(TIME_MANAGEMENT_U8_TYPE* p_context);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u16 
+ */
+u16 time_mgmnt_elapsed_u16(TIME_MANAGEMENT_U16_TYPE* p_context);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u32 
+ */
+u32 time_mgmnt_elapsed_u32(TIME_MANAGEMENT_U32_TYPE* p_context);
+
+// --------------------------------------------------------------------------------
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u8 
+ */
+u8 time_mgmnt_start_time_u8(TIME_MANAGEMENT_U8_TYPE* p_context);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u16 
+ */
+u16 time_mgmnt_start_time_u16(TIME_MANAGEMENT_U16_TYPE* p_context);
+
+/**
+ * @brief 
+ * 
+ * @param p_context 
+ * @return u32 
+ */
+u32 time_mgmnt_start_time_u32(TIME_MANAGEMENT_U32_TYPE* p_context);
 
 #endif // _TIME_MANAGEMENT_H_
