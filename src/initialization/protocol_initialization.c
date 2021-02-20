16dbe14a2c6e1da3841567ce7cc381768e6fdd9b
smuuze <sebastia_n@web.de>
2021-02-20 09:41:16 +0100

    FIX: protocol_initialization - rpi-protocol init is not called if not activeated via make-file

--- src/initialization/protocol_initialization.c
+++ src/initialization/protocol_initialization.c
@@ -25,20 +25,24 @@ void protocol_initialization(void) {
 
 	DEBUG_PASS("protocol_initialization()");
 	
-	#if defined (HOST_INTERFACE_SPI) && (HOST_INTERFACE_SPI) == 1
+	#if defined (HAS_MANAGEMENT_MODULE_RPI_PROTOCOL) && (HAS_MANAGEMENT_MODULE_RPI_PROTOCOL) == 1
 	{
-		DEBUG_PASS("protocol_initialization() - Set Hostinterface to SPI0");
-		rpi_protocol_init(i_system.driver.spi0);
-	}
-	#elif defined (HOST_INTERFACE_USART) && (HOST_INTERFACE_USART) == 1
-	{
-		DEBUG_PASS("protocol_initialization() - Set Hostinterface to USART0");
-		rpi_protocol_init(i_system.driver.usart0);
-	}
-	#elif defined (HOST_INTERFACE_I2C) && (HOST_INTERFACE_I2C) == 1
-	{
-		DEBUG_PASS("protocol_initialization() - Set Hostinterface to I2C");
-		rpi_protocol_init(i_system.driver.i2c0);
+		#if defined (HOST_INTERFACE_SPI) && (HOST_INTERFACE_SPI) == 1
+		{
+			DEBUG_PASS("protocol_initialization() - Set Hostinterface to SPI0");
+			rpi_protocol_init(i_system.driver.spi0);
+		}
+		#elif defined (HOST_INTERFACE_USART) && (HOST_INTERFACE_USART) == 1
+		{
+			DEBUG_PASS("protocol_initialization() - Set Hostinterface to USART0");
+			rpi_protocol_init(i_system.driver.usart0);
+		}
+		#elif defined (HOST_INTERFACE_I2C) && (HOST_INTERFACE_I2C) == 1
+		{
+			DEBUG_PASS("protocol_initialization() - Set Hostinterface to I2C");
+			rpi_protocol_init(i_system.driver.i2c0);
+		}
+		#endif
 	}
 	#endif
 
