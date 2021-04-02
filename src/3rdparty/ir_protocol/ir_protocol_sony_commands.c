7971d96cc01154f076448d75d2946b0ff89e218d
smuuze <sebastia_n@web.de>
2021-04-02 12:02:40 +0200

    FIX: ir_protocol_sony - using build of bit-vector to be compatibel to iso c

--- src/3rdparty/ir_protocol/ir_protocol_sony_commands.c
+++ src/3rdparty/ir_protocol/ir_protocol_sony_commands.c
@@ -30,73 +30,77 @@
 
 // --------------------------------------------------------------------------------
 
+#include "common/common_tools_bit_vector.h"
+
+// --------------------------------------------------------------------------------
+
 #include "3rdparty/ir_protocol/ir_protocol_sony.h"
 
 // --------------------------------------------------------------------------------
 
 inline void ir_protocol_sony_address_bdplayer(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
-	p_command->device   = 0b00001011;
-	p_command->extended = 0b01000111;
+	p_command->device   = BUILD_BIT_VECTOR_U8(0,0,0,0,1,0,1,1); // 0b00001011;
+	p_command->extended = BUILD_BIT_VECTOR_U8(0,1,0,0,0,1,1,1); // 0b01000111;
 }
 
 // --------------------------------------------------------------------------------
 
 inline void ir_protocol_sony_cmd_bdplayer_power(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_power()");
-	p_command->command = 0b10101000;
+	p_command->command = BUILD_BIT_VECTOR_U8(1,0,1,0,1,0,0,0); // 0b10101000;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_play(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_play()");
-	p_command->command = 0b01011000;
+	p_command->command = BUILD_BIT_VECTOR_U8(0,1,0,1,1,0,0,0); // 0b01011000;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_stop(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_stop()");
-	p_command->command = 0b00011000;
+	p_command->command = BUILD_BIT_VECTOR_U8(0,0,0,1,1,0,0,0); // 0b00011000;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_pause(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_pause()");
-	p_command->command = 0b10011000;
+	p_command->command = BUILD_BIT_VECTOR_U8(1,0,0,1,1,0,0,0); // 0b10011000;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_audio_language(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_audio_language()");
-	p_command->command = 0b00100110;
+	p_command->command = BUILD_BIT_VECTOR_U8(0,0,1,0,0,1,1,0); // 0b00100110;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_subtitle_language(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_subtitle_language()");
-	p_command->command = 0b11000110;
+	p_command->command = BUILD_BIT_VECTOR_U8(1,1,0,0,0,1,1,0); // 0b11000110;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_eject(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_eject()");
-	p_command->command = 0b01101000;
+	p_command->command = BUILD_BIT_VECTOR_U8(0,1,1,0,1,0,0,0); // 0b01101000;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_return(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_return()");
-	p_command->command = 0b11000010;
+	p_command->command = BUILD_BIT_VECTOR_U8(1,1,0,0,0,0,1,0); // 0b11000010;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_top_menu(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_top_menu()");
-	p_command->command = 0b00110100;
+	p_command->command = BUILD_BIT_VECTOR_U8(0,0,1,1,0,1,0,0); // 0b00110100;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_pop_up_menu(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_pop_up_menu()");
-	p_command->command = 0b10010100;
+	p_command->command = BUILD_BIT_VECTOR_U8(1,0,0,1,0,1,0,0); // 0b10010100;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_next(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_next()");
-	p_command->command = 0b01101010;
+	p_command->command = BUILD_BIT_VECTOR_U8(0,1,1,0,1,0,1,0); // 0b01101010;
 }
 
 inline void ir_protocol_sony_cmd_bdplayer_previous(SONY_IR_PROTOCOL_COMMAND_TYPE* p_command) {
 	DEBUG_PASS("ir_protocol_sony_cmd_bdplayer_previous()");
-	p_command->command = 0b11101010;
+	p_command->command = BUILD_BIT_VECTOR_U8(1,1,1,0,1,0,1,0); // 0b11101010;
 }
