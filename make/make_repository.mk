481de4166c20f991b59f19f81eaf68399b616865
smuuze <sebastia_n@web.de>
2021-07-16 16:00:48 +0200

    FIX: make_repository - added targets: check-for-modification and remember-framework-revision

--- make/make_repository.mk
+++ make/make_repository.mk
@@ -12,6 +12,13 @@
 GIT			= git
 GIT_FRMWRK_PATH		= -C $(FRMWRK_PATH)
 GIT_LOG			= log -1
+GIT_STATUS		= status
 
 repo_last_commit_frmwrk:
-	$(VERBOSE) $(GIT) $(GIT_FRMWRK_PATH) $(GIT_LOG) | grep commit
+	$(VERBOSE) $(GIT) $(GIT_FRMWRK_PATH) $(GIT_LOG) $(GREP) commit $(AWK) '/(commit)/ {print $$2}'
+
+repo_check_modifications_frmwrk:
+	$(VERBOSE) $(GIT) $(GIT_FRMWRK_PATH) $(GIT_STATUS) $(GREP) Changes
+
+repo_remember_frmwrk_revision: repo_check_modifications_frmwrk
+	$(VERBOSE) $(GIT) $(GIT_FRMWRK_PATH) $(GIT_LOG) $(GREP) commit $(AWK) '/(commit)/ {print $$2}' > framework_revision
