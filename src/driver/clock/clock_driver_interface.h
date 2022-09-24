72561e2841d618f390f07c6b294b645fa2a3d311
smuuze <sebastia_n@web.de>
2022-09-24 14:10:21 +0200

    MOD: clock-driver-interface - applied coding style

--- src/driver/clock/clock_driver_interface.h
+++ src/driver/clock/clock_driver_interface.h
@@ -1,34 +1,54 @@
-/*! 
- * --------------------------------------------------------------------------------
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * \file	src/driver/clock/clock_driver_interface.h
- * \author	sebastian lesse
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * --------------------------------------------------------------------------------
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * @file    clock_driver_interface.h
+ * @author  Sebastian Lesse
+ * @date    2022 / 09 / 24
+ * @brief   Interface to clock functions of the currently used cpu
+ * 
  */
 
-#ifndef _CLOCK_DRIVER_INTERFACE_H_
-#define _CLOCK_DRIVER_INTERFACE_H_
+// --------------------------------------------------------------------------------
+
+#ifndef _H_clock_driver_interface_
+#define _H_clock_driver_interface_
 
 // --------------------------------------------------------------------------------
 
-#define CLK_DRIVER_CLK_PRESCALER_1		0x00
-#define CLK_DRIVER_CLK_PRESCALER_2		0x01
-#define CLK_DRIVER_CLK_PRESCALER_4		0x02
-#define CLK_DRIVER_CLK_PRESCALER_8		0x03
-#define CLK_DRIVER_CLK_PRESCALER_16		0x04
-#define CLK_DRIVER_CLK_PRESCALER_32		0x05
-#define CLK_DRIVER_CLK_PRESCALER_64		0x06
-#define CLK_DRIVER_CLK_PRESCALER_128		0x07
-#define CLK_DRIVER_CLK_PRESCALER_256		0x08
+#define CLK_DRIVER_CLK_PRESCALER_1          0x00
+#define CLK_DRIVER_CLK_PRESCALER_2          0x01
+#define CLK_DRIVER_CLK_PRESCALER_4          0x02
+#define CLK_DRIVER_CLK_PRESCALER_8          0x03
+#define CLK_DRIVER_CLK_PRESCALER_16         0x04
+#define CLK_DRIVER_CLK_PRESCALER_32         0x05
+#define CLK_DRIVER_CLK_PRESCALER_64         0x06
+#define CLK_DRIVER_CLK_PRESCALER_128        0x07
+#define CLK_DRIVER_CLK_PRESCALER_256        0x08
 
 // --------------------------------------------------------------------------------
 
-/*
- *
- */ 
+/**
+ * @brief Initializes the clock driver of the currently used cpu.
+ * respect the order of the initialization sequence of your cpu
+ * (clock before peripherials)
+ * 
+ */
 void clock_driver_init(void);
 
 // --------------------------------------------------------------------------------
 
-#endif // _CLOCK_DRIVER_INTERFACE_H_
+#endif // _H_clock_driver_interface_
+
+// --------------------------------------------------------------------------------
