27bfe0b94182846314ffa8183fc5d36172ec2bc4
smuuze <sebastia_n@web.de>
2022-09-24 14:16:40 +0200

    MOD: cpu.h - unittest - added u64

--- src/common/cpu/unittest/cpu.h
+++ src/common/cpu/unittest/cpu.h
@@ -54,12 +54,16 @@ typedef uint16_t u16;
 
 typedef uint32_t u32;
 
+typedef uint64_t u64;
+
 
 typedef int8_t i8;
 
 typedef int16_t i16;
 
 typedef int32_t i32;
+
+typedef int64_t i64;
 
 
 typedef uint8_t ux8;
