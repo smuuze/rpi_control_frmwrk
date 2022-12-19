f9e765ad20b86bcf70899221bf7fe0163c62c85d
smuuze <sebastia_n@web.de>
2022-12-19 11:12:24 +0100

    MOD: board_pi_pico - changed pin layout of lcd-control

--- src/platine/board_PI_PICO.c
+++ src/platine/board_PI_PICO.c
@@ -20,16 +20,17 @@
  * 
  */
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 #include "driver/gpio/gpio_interface.h"
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 /**
  * @brief See section 2.19 GPIO of the datasheet
  * 
- * Following functions are available, see section 2.19.2. Function Select for more information
+ * Following functions are available, see section 2.19.2.
+ * Function Select for more information
  * 
  *  F1 - SPIn   RX / TX / CSn / SCK 
  *  F2 - UARTn  TX / RX / CTS
@@ -41,64 +42,64 @@
  * 
  */
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // PORT A
-//---------     Name,           Port,           Bit,            Pin-Cfg                                                                FUNCTION
+//---------     Name,           Port,           Bit,            Pin-Cfg                             FUNCTION
 
-    BUILD_GPIO ( GPIO_00,       GPIO_PORT_A,    GPIO_PIN_0,     GPIO_FUNCTION_2                                                 ) //    UART0 - TX
-    BUILD_GPIO ( GPIO_01,       GPIO_PORT_A,    GPIO_PIN_1,     GPIO_FUNCTION_2                                                 ) //    UART0 - RX
-    BUILD_GPIO ( GPIO_02,       GPIO_PORT_A,    GPIO_PIN_2,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_03,       GPIO_PORT_A,    GPIO_PIN_3,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_04,       GPIO_PORT_A,    GPIO_PIN_4,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) // 
-    BUILD_GPIO ( GPIO_05,       GPIO_PORT_A,    GPIO_PIN_5,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_06,       GPIO_PORT_A,    GPIO_PIN_6,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_07,       GPIO_PORT_A,    GPIO_PIN_7,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
+    BUILD_GPIO ( GPIO_00,       GPIO_PORT_A,    GPIO_PIN_0,     GPIO_FUNCTION_2             ) //    UART0 - TX
+    BUILD_GPIO ( GPIO_01,       GPIO_PORT_A,    GPIO_PIN_1,     GPIO_FUNCTION_2             ) //    UART0 - RX
+    BUILD_GPIO ( GPIO_02,       GPIO_PORT_A,    GPIO_PIN_2,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_03,       GPIO_PORT_A,    GPIO_PIN_3,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_04,       GPIO_PORT_A,    GPIO_PIN_4,     GPIO_INPUT | GPIO_IDLE_HIGH ) // 
+    BUILD_GPIO ( GPIO_05,       GPIO_PORT_A,    GPIO_PIN_5,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_06,       GPIO_PORT_A,    GPIO_PIN_6,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_07,       GPIO_PORT_A,    GPIO_PIN_7,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // PORT B
-//---------     Name,           Port,           Bit,            Pin-Cfg                                                                 FUNCTION
+//---------     Name,           Port,           Bit,            Pin-Cfg                             FUNCTION
 
-    BUILD_GPIO ( GPIO_08,       GPIO_PORT_B,    GPIO_PIN_0,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_09,       GPIO_PORT_B,    GPIO_PIN_1,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_10,       GPIO_PORT_B,    GPIO_PIN_2,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) // 
-    BUILD_GPIO ( GPIO_11,       GPIO_PORT_B,    GPIO_PIN_3,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_12,       GPIO_PORT_B,    GPIO_PIN_4,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_13,       GPIO_PORT_B,    GPIO_PIN_5,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_14,       GPIO_PORT_B,    GPIO_PIN_6,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_15,       GPIO_PORT_B,    GPIO_PIN_7,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
+    BUILD_GPIO ( GPIO_08,       GPIO_PORT_B,    GPIO_PIN_0,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_09,       GPIO_PORT_B,    GPIO_PIN_1,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_10,       GPIO_PORT_B,    GPIO_PIN_2,     GPIO_INPUT | GPIO_IDLE_HIGH ) // 
+    BUILD_GPIO ( GPIO_11,       GPIO_PORT_B,    GPIO_PIN_3,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_12,       GPIO_PORT_B,    GPIO_PIN_4,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_13,       GPIO_PORT_B,    GPIO_PIN_5,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_14,       GPIO_PORT_B,    GPIO_PIN_6,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_15,       GPIO_PORT_B,    GPIO_PIN_7,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // PORT C
-//---------     Name,           Port,           Bit,            Pin-Cfg                                                                 FUNCTION
+//---------     Name,           Port,           Bit,            Pin-Cfg                             FUNCTION
 
-    BUILD_GPIO ( GPIO_16,       GPIO_PORT_C,    GPIO_PIN_0,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_17,       GPIO_PORT_C,    GPIO_PIN_1,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_18,       GPIO_PORT_C,    GPIO_PIN_2,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_19,       GPIO_PORT_C,    GPIO_PIN_3,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_20,       GPIO_PORT_C,    GPIO_PIN_4,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_21,       GPIO_PORT_C,    GPIO_PIN_5,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_22,       GPIO_PORT_C,    GPIO_PIN_6,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) // 
-    BUILD_GPIO ( GPIO_23,       GPIO_PORT_C,    GPIO_PIN_7,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) // 
+    BUILD_GPIO ( GPIO_16,       GPIO_PORT_C,    GPIO_PIN_0,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_17,       GPIO_PORT_C,    GPIO_PIN_1,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_18,       GPIO_PORT_C,    GPIO_PIN_2,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_19,       GPIO_PORT_C,    GPIO_PIN_3,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_20,       GPIO_PORT_C,    GPIO_PIN_4,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_21,       GPIO_PORT_C,    GPIO_PIN_5,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_22,       GPIO_PORT_C,    GPIO_PIN_6,     GPIO_INPUT | GPIO_IDLE_HIGH ) // 
+    BUILD_GPIO ( GPIO_23,       GPIO_PORT_C,    GPIO_PIN_7,     GPIO_INPUT | GPIO_IDLE_HIGH ) // 
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // PORT D
-//---------     Name,           Port,           Bit,            Pin-Cfg                                                                 FUNCTION
+//---------     Name,           Port,           Bit,            Pin-Cfg                             FUNCTION
 
-    BUILD_GPIO ( GPIO_24,       GPIO_PORT_D,    GPIO_PIN_0,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_25,       GPIO_PORT_D,    GPIO_PIN_1,     GPIO_OUTPUT | GPIO_IDLE_HIGH                                    ) //    ON_BOARD LED
-    BUILD_GPIO ( GPIO_26,       GPIO_PORT_D,    GPIO_PIN_2,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_27,       GPIO_PORT_D,    GPIO_PIN_3,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
-    BUILD_GPIO ( GPIO_28,       GPIO_PORT_D,    GPIO_PIN_4,     GPIO_INPUT | GPIO_IDLE_HIGH                                     ) //
+    BUILD_GPIO ( GPIO_24,       GPIO_PORT_D,    GPIO_PIN_0,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_25,       GPIO_PORT_D,    GPIO_PIN_1,     GPIO_OUTPUT | GPIO_IDLE_HIGH ) //   ON_BOARD LED
+    BUILD_GPIO ( GPIO_26,       GPIO_PORT_D,    GPIO_PIN_2,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_27,       GPIO_PORT_D,    GPIO_PIN_3,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
+    BUILD_GPIO ( GPIO_28,       GPIO_PORT_D,    GPIO_PIN_4,     GPIO_INPUT | GPIO_IDLE_HIGH ) //
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 // GPIO Aliases for LCD
 
-GPIO_ALIAS (LCD_EN, GPIO_16 )
-GPIO_ALIAS (LCD_RS, GPIO_17 )
-GPIO_ALIAS (LCD_D7, GPIO_18 )
-GPIO_ALIAS (LCD_D6, GPIO_19 )
-GPIO_ALIAS (LCD_D5, GPIO_20 )
-GPIO_ALIAS (LCD_D4, GPIO_21 )
+GPIO_ALIAS (LCD_EN, GPIO_14 )
+GPIO_ALIAS (LCD_RS, GPIO_15 )
+GPIO_ALIAS (LCD_D7, GPIO_10 )
+GPIO_ALIAS (LCD_D6, GPIO_11 )
+GPIO_ALIAS (LCD_D5, GPIO_12 )
+GPIO_ALIAS (LCD_D4, GPIO_13 )
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
