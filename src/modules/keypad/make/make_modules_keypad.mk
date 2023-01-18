81c32509e28fb57747c39f9769e8c6ea5417c62f
smuuze <sebastia_n@web.de>
2023-01-18 17:13:18 +0100

    MOD: make_module_keypad - remnoved useless statements

--- src/modules/keypad/make/make_modules_keypad.mk
+++ src/modules/keypad/make/make_modules_keypad.mk
@@ -7,8 +7,6 @@ MODULE_KEYPAD_PATH = $(APP_PATH)/modules/keypad
 ifneq '' '$(findstring KEYPAD_3X4,$(MODULES_CFG))'
 	DEFS += -D KEYPAD_3x4_CONTROLLER_AVAILABLE=1
 	CSRCS += $(MODULE_KEYPAD_PATH)/keypad_controller_3x4.c
-
-	DRIVER_MODULE_CFG += KEYPAD_3x4
 endif
 
 # -----------------------------------------------------------------------
