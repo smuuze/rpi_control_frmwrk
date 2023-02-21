86fba53509c1dd2a5bb3a06474f87c975d90cbb9
smuuze <sebastia_n@web.de>
2023-02-21 20:43:04 +0100

    FIX: keypad_to_lcd_task - using SIGNAL_LCD_LINE_send to update lecd text

--- src/app_tasks/keypad_to_lcd_task.c
+++ src/app_tasks/keypad_to_lcd_task.c
@@ -344,7 +344,7 @@ static MCU_TASK_INTERFACE_TASK_STATE KEYPAD_TO_LCD_task_get_state(void) {
  * @see  mcu_task_management/mcu_task_interface.h#MCU_TASK_INTERFACE.run
  */
 static void KEYPAD_TO_LCD_TASK_execute(void) {
-    LCD_PRINT(lcd_text);
+    SIGNAL_LCD_LINE_send(lcd_text);
     lcd2key_is_active = 0;
 }
 
@@ -378,7 +378,7 @@ void key_2_lcd_init(void) {
         lcd_text[i] = ' ';
     }
 
-    LCD_INIT();
+    lcd_set_enabled(LCD_ENABLE);
 
     KEY2LCD_KEY0_PRESSED_SLOT_connect();
     KEY2LCD_KEY1_PRESSED_SLOT_connect();
