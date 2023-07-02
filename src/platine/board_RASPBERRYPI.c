3f9fbcd34e74b06e0f0368008ec6855fb7a77b0f
smuuze <sebastia_n@web.de>
2023-07-02 09:06:50 +0200

    FIX: RPi-Board - correct gpio-pin for pir-sensor

--- src/platine/board_RASPBERRYPI.c
+++ src/platine/board_RASPBERRYPI.c
@@ -33,61 +33,61 @@
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 // PORT A
-//---------     Name,           Port,           Bit,        Pin-Cfg                             SHC             wiring-Pi   HiFi-Berry DAC+
+//---------     Name,           Port,           Bit,        Pin-Cfg                                 SHC             wiring-Pi   HiFi-Berry DAC+
 
 //  BUILD_GPIO ( 3V3,           GPIO_PORT_A,    GPIO_PIN_0,    UNUSEABLE                   )
 //  BUILD_GPIO ( 5V,            GPIO_PORT_A,    GPIO_PIN_1,    UNUSEABLE                   )
-    BUILD_GPIO ( I2C_SDA,       GPIO_PORT_A,    GPIO_PIN_2,    GPIO_DEACTIVATE             ) //    I2C_SDA         GPIO_08     ALT0
-//     BUILD_GPIO ( 5V,            GPIO_PORT_A,    GPIO_PIN_3,    UNUSEABLE                   ) //
-    BUILD_GPIO ( I2C_SCL,       GPIO_PORT_A,    GPIO_PIN_4,    GPIO_DEACTIVATE             ) //    I2C_SCL         GPIO_09     ALT0
+    BUILD_GPIO ( I2C_SDA,       GPIO_PORT_A,    GPIO_PIN_2,    GPIO_DEACTIVATE             ) //    I2C_SDA          GPIO_08      ALT0
+//  BUILD_GPIO ( 5V,            GPIO_PORT_A,    GPIO_PIN_3,    UNUSEABLE                   ) //
+    BUILD_GPIO ( I2C_SCL,       GPIO_PORT_A,    GPIO_PIN_4,    GPIO_DEACTIVATE             ) //    I2C_SCL          GPIO_09      ALT0
 //  BUILD_GPIO ( GND,           GPIO_PORT_A,    GPIO_PIN_5,    UNUSEABLE                   ) //
-    BUILD_GPIO ( GPIO04,        GPIO_PORT_A,    GPIO_PIN_6,    GPIO_INPUT | GPIO_IDLE_HIGH ) //    GPIO04          GPIO_07
+    BUILD_GPIO ( GPIO04,        GPIO_PORT_A,    GPIO_PIN_6,    GPIO_INPUT | GPIO_IDLE_HIGH ) //    GPIO04           GPIO_07
     BUILD_GPIO ( UART_TXD,      GPIO_PORT_A,    GPIO_PIN_7,    GPIO_DEACTIVATE             ) //    GPIO_15
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 // PORT B
-//---------     Name,           Port,           Bit,        Pin-Cfg                             SHC             wiring-Pi   HiFi-Berry DAC+
+//---------     Name,           Port,           Bit,        Pin-Cfg                                 SHC             wiring-Pi   HiFi-Berry DAC+
 
 //  BUILD_GPIO ( GND,           GPIO_PORT_B,    GPIO_PIN_0,    UNUSEABLE                   ) //
-    BUILD_GPIO ( UART_RXD,      GPIO_PORT_B,    GPIO_PIN_1,    GPIO_DEACTIVATE             ) //    DEBUG_RX        GPIO_16
-    BUILD_GPIO ( GPIO17,        GPIO_PORT_B,    GPIO_PIN_2,    GPIO_INPUT                  ) //    REQUEST_CLIENT  GPIO_00
-    BUILD_GPIO ( GPIO18,        GPIO_PORT_B,    GPIO_PIN_3,    GPIO_DEACTIVATE             ) //    DEACTIVATED     GPIO_01     ALT0
-    BUILD_GPIO ( GPIO27,        GPIO_PORT_B,    GPIO_PIN_4,    GPIO_INPUT | GPIO_IDLE_HIGH    ) //    KEYPAD_COL_3    GPIO_02
+    BUILD_GPIO ( UART_RXD,      GPIO_PORT_B,    GPIO_PIN_1,    GPIO_DEACTIVATE             ) //     DEBUG_RX        GPIO_16
+    BUILD_GPIO ( GPIO17,        GPIO_PORT_B,    GPIO_PIN_2,    GPIO_INPUT                  ) //     REQUEST_CLIENT  GPIO_00
+    BUILD_GPIO ( GPIO18,        GPIO_PORT_B,    GPIO_PIN_3,    GPIO_DEACTIVATE             ) //     DEACTIVATED     GPIO_01     ALT0
+    BUILD_GPIO ( GPIO27,        GPIO_PORT_B,    GPIO_PIN_4,    GPIO_INPUT | GPIO_IDLE_HIGH ) //     KEYPAD_COL_3    GPIO_02
 //  BUILD_GPIO ( GND,           GPIO_PORT_B,    GPIO_PIN_5,    UNUSEABLE                   ) //
-    BUILD_GPIO ( GPIO22,        GPIO_PORT_B,    GPIO_PIN_6,    GPIO_INPUT | GPIO_IDLE_HIGH    ) //    KEYPAD_COL_2    GPIO_03
-    BUILD_GPIO ( GPIO23,        GPIO_PORT_B,    GPIO_PIN_7,    GPIO_INPUT | GPIO_IDLE_HIGH    ) //    KEYPAD_COL_1    GPIO_04
+    BUILD_GPIO ( GPIO22,        GPIO_PORT_B,    GPIO_PIN_6,    GPIO_INPUT | GPIO_IDLE_HIGH ) //     KEYPAD_COL_2    GPIO_03
+    BUILD_GPIO ( GPIO23,        GPIO_PORT_B,    GPIO_PIN_7,    GPIO_INPUT | GPIO_IDLE_HIGH ) //     KEYPAD_COL_1    GPIO_04
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 // PORT C
-//---------     Name,           Port,           Bit,        Pin-Cfg                             SHC             wiring-Pi   HiFi-Berry DAC+
+//---------     Name,           Port,           Bit,        Pin-Cfg                                 SHC             wiring-Pi   HiFi-Berry DAC+
 
 //  BUILD_GPIO ( 3V3,           GPIO_PORT_C,    GPIO_PIN_0, UNUSEABLE                   ) //
-    BUILD_GPIO ( GPIO24,        GPIO_PORT_C,    GPIO_PIN_1, GPIO_INPUT | GPIO_IDLE_HIGH ) //    KEYPAD_ROW_1    GPIO_05
-    BUILD_GPIO ( SPI_MOSI,      GPIO_PORT_C,    GPIO_PIN_2, GPIO_DEACTIVATE             ) //    SPI_MOSI        GPIO_12
+    BUILD_GPIO ( GPIO24,        GPIO_PORT_C,    GPIO_PIN_1, GPIO_INPUT | GPIO_IDLE_HIGH ) //        KEYPAD_ROW_1    GPIO_05
+    BUILD_GPIO ( SPI_MOSI,      GPIO_PORT_C,    GPIO_PIN_2, GPIO_DEACTIVATE             ) //        SPI_MOSI        GPIO_12
 //  BUILD_GPIO ( GND,           GPIO_PORT_C,    GPIO_PIN_3, UNUSEABLE                   ) //
-    BUILD_GPIO ( SPI_MISO,      GPIO_PORT_C,    GPIO_PIN_4, GPIO_DEACTIVATE             ) //    SPI_MISO        GPIO_13
-    BUILD_GPIO ( GPIO25,        GPIO_PORT_C,    GPIO_PIN_5, GPIO_INPUT | GPIO_IDLE_HIGH    ) //    KEYPAD_ROW_2    GPIO_06
-    BUILD_GPIO ( SPI_SCLK,      GPIO_PORT_C,    GPIO_PIN_6, GPIO_DEACTIVATE             ) //    SPI_SCLK        GPIO_14
-    BUILD_GPIO ( SPI_CE0,       GPIO_PORT_C,    GPIO_PIN_7, GPIO_DEACTIVATE             ) //    SPI0_CS         GPIO_10
+    BUILD_GPIO ( SPI_MISO,      GPIO_PORT_C,    GPIO_PIN_4, GPIO_DEACTIVATE             ) //        SPI_MISO        GPIO_13
+    BUILD_GPIO ( GPIO25,        GPIO_PORT_C,    GPIO_PIN_5, GPIO_INPUT | GPIO_IDLE_HIGH ) //        KEYPAD_ROW_2    GPIO_06
+    BUILD_GPIO ( SPI_SCLK,      GPIO_PORT_C,    GPIO_PIN_6, GPIO_DEACTIVATE             ) //        SPI_SCLK        GPIO_14
+    BUILD_GPIO ( SPI_CE0,       GPIO_PORT_C,    GPIO_PIN_7, GPIO_DEACTIVATE             ) //        SPI0_CS         GPIO_10
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 // PORT D
-//---------     Name,           Port,           Bit,        Pin-Cfg                             SHC             wiring-Pi   HiFi-Berry DAC+
+//---------     Name,           Port,           Bit,        Pin-Cfg                                 SHC             wiring-Pi   HiFi-Berry DAC+
 
 //  BUILD_GPIO ( GND,           GPIO_PORT_D,    GPIO_PIN_0,    UNUSEABLE                   ) //
-    BUILD_GPIO ( SPI_CE1,       GPIO_PORT_D,    GPIO_PIN_1,    GPIO_DEACTIVATE             ) //    SPI_CE1         GPIO_11
+    BUILD_GPIO ( SPI_CE1,       GPIO_PORT_D,    GPIO_PIN_1,    GPIO_DEACTIVATE             ) //     SPI_CE1         GPIO_11
 //  BUILD_GPIO ( ID_SD,         GPIO_PORT_D,    GPIO_PIN_2,    UNUSEABLE                   ) //
 //  BUILD_GPIO ( ID_SC,         GPIO_PORT_D,    GPIO_PIN_3,    UNUSEABLE                   ) //
-    BUILD_GPIO ( GPIO05,        GPIO_PORT_D,    GPIO_PIN_4,    GPIO_INPUT | GPIO_IDLE_HIGH    ) //    KEYPAD_ROW_3    GPIO_21
+    BUILD_GPIO ( GPIO05,        GPIO_PORT_D,    GPIO_PIN_4,    GPIO_INPUT | GPIO_IDLE_HIGH ) //     KEYPAD_ROW_3    GPIO_21
 //  BUILD_GPIO ( GND,           GPIO_PORT_D,    GPIO_PIN_5,    UNUSEABLE                   ) /
-    BUILD_GPIO ( GPIO06,        GPIO_PORT_D,    GPIO_PIN_6,    GPIO_INPUT | GPIO_IDLE_HIGH    ) //    KEYPAD_ROW_4    GPIO_22
-    BUILD_GPIO ( GPIO12,        GPIO_PORT_D,    GPIO_PIN_7,    GPIO_INPUT | GPIO_IDLE_HIGH    ) //    LCD_D4          GPIO_26
+    BUILD_GPIO ( GPIO06,        GPIO_PORT_D,    GPIO_PIN_6,    GPIO_INPUT | GPIO_IDLE_HIGH ) //     KEYPAD_ROW_4    GPIO_22
+    BUILD_GPIO ( GPIO12,        GPIO_PORT_D,    GPIO_PIN_7,    GPIO_INPUT | GPIO_IDLE_HIGH ) //     LCD_D4          GPIO_26
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
 // PORT E
-//---------     Name,           Port,           Bit,        Pin-Cfg                                                 SHC     wiring-Pi   HiFi-Berry DAC+
+//---------     Name,           Port,           Bit,        Pin-Cfg                                                     SHC     wiring-Pi   HiFi-Berry DAC+
 
-    BUILD_GPIO ( GPIO13,        GPIO_PORT_E,    GPIO_PIN_0,    GPIO_INPUT | GPIO_IDLE_HIGH ) //                                GPIO_23
+    BUILD_GPIO ( GPIO13,        GPIO_PORT_E,    GPIO_PIN_0,    GPIO_INPUT | GPIO_IDLE_HIGH ) //                         PIR     GPIO_23
 //  BUILD_GPIO ( GND,           GPIO_PORT_E,    GPIO_PIN_1,    UNUSEABLE                   ) //     
     BUILD_GPIO ( GPIO19,        GPIO_PORT_E,    GPIO_PIN_2,    GPIO_DEACTIVATE | GPIO_INPUT | GPIO_IDLE_HIGH    ) //    LCD_RS  GPIO_24     ALT0
     BUILD_GPIO ( GPIO16,        GPIO_PORT_E,    GPIO_PIN_3,    GPIO_DEACTIVATE | GPIO_INPUT | GPIO_IDLE_HIGH    ) //    LCD_D5  GPIO_27
@@ -156,6 +156,6 @@
 
 // ALIAS - PIR SENSOR
 
-    GPIO_ALIAS (    PIR_SENSOR,     GPIO_07 )
+    GPIO_ALIAS (    PIR_SENSOR,     GPIO13 )
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------
